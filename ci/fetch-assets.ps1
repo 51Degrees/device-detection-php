bde9fbbf9451ba591a2e4808f5657b3d59f6d230
Alexander Popel <alexander.popel@postindustria.com>
2025-12-04 13:21:58 +0200

    ci: use common fetch-assets script

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,29 +1,12 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
     [string]$DeviceDetection,
     [string]$CsvUrl
 )
 
 if ($env:GITHUB_JOB -eq "PreBuild") {
-    Write-Output "Skipping assets fetching"
+    Write-Host "Skipping assets fetching"
     exit 0
 }
 
-$assets = New-Item -ItemType Directory -Path assets -Force
-$file = "51Degrees.csv"
-
-if (!(Test-Path $assets/$file)) {
-    if (!$DeviceDetection) {
-        Write-Output "::warning file=$($MyInvocation.ScriptName),line=$($MyInvocation.ScriptLineNumber),title=No License Key::A device detection license was not provided, so '$file' will not be downloaded."
-        return
-    }
-    Write-Output "Downloading $file"
-    ./steps/fetch-csv-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $CsvUrl
-    Get-Content -TotalCount 1 $RepoName/51Degrees-Tac/51Degrees-Tac-All.csv | Out-File $assets/$file # We only need a header
-    Remove-Item -Path $RepoName/51Degrees-Tac.zip, $RepoName/51Degrees-Tac/51Degrees-Tac-All.csv
-} else {
-    Write-Output "'$file' exists, skipping download"
-}
-
-New-Item -ItemType SymbolicLink -Force -Target "$assets/$file" -Path "$RepoName/tests/$file"
+./steps/fetch-assets.ps1 -DeviceDetection:$DeviceDetection -CsvUrl:$CsvUrl -Assets "51Degrees.csv"
+New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/51Degrees.csv" -Path "$PSScriptRoot/../tests/51Degrees.csv"
